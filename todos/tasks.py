3dac7350c5a5bf614e79284d174a1d54139a7d43
Santiago Basulto <santiago.basulto@gmail.com>
2018-02-28 21:31:07 -0300

    Final implementation

--- todos/tasks.py
+++ todos/tasks.py
@@ -6,16 +6,55 @@ from .utils import parse_date, parse_int
 
 
 def new():
-    pass
+    return []
 
 
 def create_task(tasks, name, description=None, due_on=None):
-    pass
+    if due_on and type(due_on) != datetime:
+        due_on = parse_date(due_on)
+
+    task = {
+        'task': name,
+        'description': description,
+        'due_on': due_on,
+        'status': 'pending'
+    }
+    tasks.append(task)
 
 
 def list_tasks(tasks, status='all'):
-    pass
+    if status not in ('all', 'pending', 'done'):
+        raise InvalidTaskStatus()
+
+    task_list = []
+    for idx, task in enumerate(tasks, 1):
+        if task['due_on'] is not None:
+            due_on = task['due_on'].strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            due_on = None
+
+        t = (idx, task['task'], due_on, task['status'])
+        if status == 'all' or task['status'] == status:
+            task_list.append(t)
+
+    return task_list
 
 
 def complete_task(tasks, name):
-    pass
+    # Name can be an int
+    idx = parse_int(name)
+
+    new_tasks = []
+    found = False
+
+    for task_id, task in enumerate(tasks, start=1):
+        if name == task['task'] or idx == task_id:
+            if task['status'] == 'done':
+                raise TaskAlreadyDoneException()
+            task = task.copy()
+            task['status'] = 'done'
+            found = True
+        new_tasks.append(task)
+    if not found:
+        raise TaskDoesntExistException()
+    return new_tasks
--- todos/utils.py
+++ todos/utils.py
@@ -4,20 +4,57 @@ from .exceptions import InvalidTaskDueDateException
 
 
 def parse_date(date_str):
-    pass
+    formats = [
+        '%Y-%m-%d',
+        '%Y-%m-%d %H:%M:%S',
+    ]
+    for format in formats:
+        try:
+            return datetime.strptime(date_str, format)
+        except ValueError:
+            pass
+
+    raise InvalidTaskDueDateException()
 
 
 def parse_int(value):
-    pass
+    try:
+        return int(value)
+    except ValueError:
+        return None
 
 
 def serialize(tasks):
-    pass
+    new_tasks = []
+    for task in tasks:
+        task = task.copy()
+        task['due_on'] = task['due_on'].strftime('%Y-%m-%d %H:%M:%S')
+        new_tasks.append(task)
+    return new_tasks
 
 
 def unserialize(blob):
-    pass
+    raw_tasks = json.loads(blob)
+    new_tasks = []
+    for task in raw_tasks:
+        task = task.copy()
+        if task['due_on']:
+            task['due_on'] = parse_date(task['due_on'])
+        new_tasks.append(task)
+    return new_tasks
 
 
 def summary(tasks):
-    pass
+    summary_obj = {
+        'total': len(tasks),
+        'pending': 0,
+        'done': 0,
+    }
+    for task in tasks:
+        if task['status'] == 'pending':
+            summary_obj['pending'] += 1
+
+        if task['status'] == 'done':
+            summary_obj['done'] += 1
+
+    return summary_obj
