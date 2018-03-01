874bf5a786de541c601dbb88f17bf3b2fde2db49
Santiago Basulto <santiago.basulto@gmail.com>
2018-02-28 21:23:37 -0300

    Student tasks

--- todos/tasks.py
+++ todos/tasks.py
@@ -6,16 +6,45 @@ from .utils import parse_date, parse_int
 
 
 def new():
-    pass
+    return []
 
 
 def create_task(tasks, name, description=None, due_on=None):
-    pass
+    if due_on and type(due_on) != datetime:
+        due_on = parse_date(due_on)
+
+    task = {
+        'task': name,
+        'description': description,
+        'due_on': due_on,
+        'status': 'pending'
+    }
+    tasks.append(task)
 
 
 def list_tasks(tasks, status='all'):
-    pass
+    task_list = []
+    for idx, task in enumerate(tasks, 1):
+        if task['due_on'] is not None:
+            due_on = task['due_on'].strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            due_on = None
+
+        t = (idx, task['task'], due_on, task['status'])
+        if status == 'all' or task['status'] == status:
+            task_list.append(t)
+
+    return task_list
 
 
 def complete_task(tasks, name):
-    pass
+    new_tasks = []
+
+    for task in tasks:
+        if name == task['task']:
+            task = task.copy()
+            task['status'] = 'done'
+        new_tasks.append(task)
+
+    return new_tasks
+
--- todos/utils.py
+++ todos/utils.py
@@ -4,7 +4,17 @@ from .exceptions import InvalidTaskDueDateException
 
 
 def parse_date(date_str):
-    pass
+    formats = [
+        '%Y-%m-%d',
+        '%Y-%m-%d %H:%M:%S',
+    ]
+    if date_str is None:
+        return date_str
+    for format in formats:
+        try:
+            return datetime.strptime(date_str, format)
+        except ValueError:
+            pass
 
 
 def parse_int(value):
@@ -12,12 +22,35 @@ def parse_int(value):
 
 
 def serialize(tasks):
-    pass
+    new_tasks = []
+    for task in tasks:
+        task = task.copy()
+        task['due_on'] = task['due_on'].strftime('%Y-%m-%d %H:%M:%S')
+        new_tasks.append(task)
+    return new_tasks
 
 
 def unserialize(blob):
-    pass
+    raw_tasks = json.loads(blob)
+    new_tasks = []
+    for task in raw_tasks:
+        task = task.copy()
+        task['due_on'] = parse_date(task['due_on'])
+        new_tasks.append(task)
+    return new_tasks
 
 
 def summary(tasks):
-    pass
+    summary_obj = {
+        'total': len(tasks),
+        'pending': 0,
+        'done': 0,
+    }
+    for task in tasks:
+        if task['status'] == 'pending':
+            summary_obj['pending'] += 1
+
+        if task['status'] == 'done':
+            summary_obj['done'] += 1
+
+    return summary_obj
